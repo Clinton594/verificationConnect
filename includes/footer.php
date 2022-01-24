4975bff109d8fe403176d794a3962d805690c748
Onuigbo Clinton <32768102+Clinton594@users.noreply.github.com>
2022-01-24 23:04:15 +0100

    particlesjs

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
   <particles style="display: none;"><?= _readFile("js/particlesjs-config.json") ?></particles>
   <script src="<?= $uri->backend ?>js/jquery-3.3.1.min.js"></script>
   <script src="<?= $uri->backend ?>js/controllers.js"></script>
-  <script src="<?= $uri->site ?>js/particles.js-master/particles.min.js"></script>
+  <script src="<?= $uri->site ?>js/particlesjs-master/particles.min.js"></script>
   <script>
     $(document).ready(function() {
       let particles = $("particles").text();

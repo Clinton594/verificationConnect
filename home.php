36f3b77c221b671fb478555d8ee6c4880a442a5f
Onuigbo Clinton <32768102+Clinton594@users.noreply.github.com>
2022-04-14 08:14:36 +0100

    Added particles js

--- home.php
+++ home.php
@@ -1827,6 +1827,15 @@
       width: 100%;
     }
 
+    #particles-js {
+      position: fixed;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      z-index: 0;
+    }
+
     .footer {
       background: #000000;
       display: -webkit-box;
@@ -2069,9 +2078,12 @@
 
 <body data-new-gr-c-s-check-loaded="14.1050.0" data-gr-ext-installed="">
   <noscript>The website doesn't work in your browser</noscript>
+
   <main id="main">
     <div class="scrollable-body">
       <div class="dark-bg page-tips">
+        <div id="particles-js"></div>
+
         <div class="page-title-block">
           <div class="page-title-header">
             <div class="page-title-header--emoji1"></div>
@@ -2155,6 +2167,124 @@
   </main>
   <script src="master/js/jquery-3.3.1.min.js"></script>
   <script src="master/js/controllers.js"></script>
+  <script src="<?= $uri->site ?>js/particles.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      // let particles = $("particles").text();
+      // $("particles").remove();
+
+      particlesJS("particles-js", {
+        "particles": {
+          "number": {
+            "value": 80,
+            "density": {
+              "enable": true,
+              "value_area": 800
+            }
+          },
+          "color": {
+            "value": "#ffffff"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 0,
+              "color": "#000000"
+            },
+            "polygon": {
+              "nb_sides": 5
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "repulse"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "push"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 200,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true
+      });
+    })
+  </script>
   <script>
     $(document).ready(function() {
       $(".tip-amount").click(function() {
